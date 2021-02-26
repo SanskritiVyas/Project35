53e6fec2a6f57a9de0d3effd6f1236fd8081c53b
SanskritiVyas <76947177+SanskritiVyas@users.noreply.github.com>
2021-02-26 14:31:50 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var hotairballoon;
+var database;
+var position;
+function setup(){
+    database=firebase.database()
+    createCanvas(500,500);
+    ball = createSprite(250,250,10,10);
+    ball.shapeColor = "red";
+var ballposition=database.ref('ball/position')
+ballposition.on("value",readPosition,showError)
+}
+
+function draw(){
+    background("white");
+    if(position!==undefined){
+    if(keyDown(LEFT_ARROW)){
+        writePosition(-1,0);
+    }
+    else if(keyDown(RIGHT_ARROW)){
+        writePosition(1,0);
+    }
+    else if(keyDown(UP_ARROW)){
+        writePosition(0,-1);
+    }
+    else if(keyDown(DOWN_ARROW)){
+        writePosition(0,+1);
+    }
+    drawSprites();
+}
+}
+function writePosition(x,y){
+ database.ref('hotairballoon/position').set({
+    'x':position.x+x,
+    'y':position.y+y
+ })
+}
+
+function readPosition(data){
+position=data.val()
+hotairballoonposition.x=position.x
+hotairballoonposition.y=position.y
+}
+
+function showError(){
+console.log('error')
+}
